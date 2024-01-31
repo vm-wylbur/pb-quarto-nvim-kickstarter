eb591fd8d881001fdbdc1d4cda9e18ee855ca8af
Patrick Ball <pball@hrdag.org>
2024-01-31 08:33:48 -0800

    minor tuning

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -711,23 +711,23 @@ return {
 
   -- paste an image to markdown from the clipboard
   -- :PasteImg,
-  {
-    "dfendr/clipboard-image.nvim",
-    keys = {
-      { "<leader>ip", ":PasteImg<cr>", desc = "image paste" },
-    },
-    cmd = {
-      "PasteImg",
-    },
-    config = function()
-      require 'clipboard-image'.setup {
-        quarto = {
-          img_dir = "img",
-          affix = "![](%s)"
-        }
-      }
-    end
-  },
+  -- {
+  --   "dfendr/clipboard-image.nvim",
+  --   keys = {
+  --     { "<leader>ip", ":PasteImg<cr>", desc = "image paste" },
+  --   },
+  --   cmd = {
+  --     "PasteImg",
+  --   },
+  --   config = function()
+  --     require 'clipboard-image'.setup {
+  --       quarto = {
+  --         img_dir = "img",
+  --         affix = "![](%s)"
+  --       }
+  --     }
+  --   end
+  -- },
 
   -- preview equations
   {
